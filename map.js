88b702adbb5bab2139b57c9fd8c58f024290346d
David Luc <luc.ky@me.com>
2023-04-06 21:57:53 -0400

    Initial commit for function that maps a new array based on the results of the ccallback function

---
+++ map.js
@@ -0,0 +1,37 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed:  ${array1} !== ${array2}`);
+  }
+};
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+const results1 = map(words, word => word[0]);
+
+assertArraysEqual(results1, ['g','c','t','m','t']);
+assertArraysEqual(map(words, word => word[1]), ['r','o','o','a','o']);
+assertArraysEqual(map(words, word => word.length), [6, 7, 2, 5, 3]);
+assertArraysEqual(map(words, word => word[word.length - 1]), ['d','l','o','r','m']);
