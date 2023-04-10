51b88a2adbf4a7b3e2683249694ddf17fcb70968
David Luc <luc.ky@me.com>
2023-04-10 15:42:29 -0400

    Added modules, removed testing

--- middle.js
+++ middle.js
@@ -1,25 +1,3 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-
-  return true;
-};
-
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed:  ${array1} !== ${array2}`);
-  }
-};
-
 const middle = function(array) {
   const midIndex = Math.floor(array.length / 2);
 
@@ -32,20 +10,4 @@ const middle = function(array) {
   }
 };
 
-const testArray1 = [1];
-assertArraysEqual(middle(testArray1), []);
-
-const testArray2 = [1, 2];
-assertArraysEqual(middle(testArray2), []);
-
-const testArray3 = [1, 2, 3];
-assertArraysEqual(middle(testArray3), [2]);
-
-const testArray4 = [1, 2, 3, 4, 5];
-assertArraysEqual(middle(testArray4), [3]);
-
-const testArray5 = [1, 2, 3, 4];
-assertArraysEqual(middle(testArray5), [2, 3]);
-
-const testArray6 = [1, 2, 3, 4, 5, 6];
-assertArraysEqual(middle(testArray6), [3, 4]);
+module.exports = middle;
