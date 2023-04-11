75d66ea76decfff3ba09e0837ea675a5846fc4e7
David Luc <luc.ky@me.com>
2023-04-11 00:10:41 -0400

    Added modules, removed testing

--- countLetters.js
+++ countLetters.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const countLetters = function(sentence) {
   const letterCount = {};
 
@@ -22,4 +14,4 @@ const countLetters = function(sentence) {
   return letterCount;
 };
 
-console.log(countLetters("lighthouse in the house"));
+module.exports = countLetters;
